ec2f55aa411e08667e20156a4c818dc0a9782dbf
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2024-05-30 11:03:30 +0530

    Update task1888.js

--- tasks/task1888.js
+++ tasks/task1888.js
@@ -1,46 +1,104 @@
-function isObject(obj) {
-    return obj !== null && typeof obj === 'object';
-}
+// Write an “assertObjectsEqual” function from scratch which take two object and string as parameter and return string.
+// The examples below represent different use cases.
 
-function deepEqual(obj1, obj2) {
-    if (obj1 === obj2) {
-        return true;
-    }
+// var expected = {foo: 6, bar: 5};
+// var actual = {foo: 5, bar: 6}
+// assertObjectsEqual(actual, expected, ‘detects that two objects are equal’);
+// output => True
 
-    // both should be objects => edge case
-    if ((!isObject(obj1)) || (!isObject(obj2))) return false;
 
-    const keysObj1 = Object.keys(obj1); // get all keys of obj1
-    const keysObj2 = Object.keys(obj2); // get all keys of obj2
 
-    // the no of keys of both the objects should be same
-    if (keysObj1.length !== keysObj2.length) return false; 
 
-    // for nested obejcts, we have to recursivly check for the values
-    for (let key of keysObj1) {
-        if ( !keysObj2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
-            return false; // the value 
+// const _ = require('lodash');
+
+// function assertObjectsEqual(actualObj, expectedObj, str){
+//     console.log(_.isEqual(actualObj, expectedObj));
+// }
+
+
+// function compare(obj1, obj2){
+//     var x = (obj1.sort());
+//     return JSON.stringify(obj1) == JSON.stringify(obj2);
+// }
+
+
+// function assertObjectsEqual(actualObj, expectedObj, str){
+//     return compare(actualObj,expectedObj);
+// }
+
+function assertObjectsEqual(actual, expected, message) {
+
+    // function to compare the two objects 
+    const isEqual = (obj1, obj2) => {
+        const keys1 = Object.keys(obj1);
+        const keys2 = Object.keys(obj2);
+
+        // check if the number of keys are equal
+        if (keys1.length !== keys2.length) {
+            return false;
         }
-    }
 
-    return true;
-}
+        // use keys of obj1, and simultaneouly check for the correspoding key of object 2
+        for (const key of keys1) {
 
-function assertObjectsEqual(actual, expected, description) {
-    if (deepEqual(actual, expected)) {
-        console.log("Passed !");
+            // check if the value of the key is another nested object
+            if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+                if (!isEqual(obj1[key], obj2[key])) {
+                    
+                    // goes till the last nested obejct and checks
+
+                    return false;
+                }
+
+            } 
+            // if the values of keys are not equal
+            else if (obj1[key] !== obj2[key]) {
+
+                //return false if values of the keys is not equal
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+
+    if (isEqual(actual, expected)) {
+        console.log("Passed");
     } else {
-        console.log("Failed !");
+        console.log(`failed as expected is ${JSON.stringify(expected)}, but got ${JSON.stringify(actual)}`);
     }
 }
 
 
+function main(){
+    // var expected = {foo: 5, bar: {
+    //     name : "prasanna",
+    //     age : 22
+    // }};
+    // var actual = {foo: 5, bar: {
+    //     name : "prasanna",
+    //     age : 90
+    // }}
 
-var expected1 = { foo: 5, bar: 6 };
-var actual1 = { foo: 5, bar: 6 };
+    var expected = {
+        foo : "5",
+        bar : "6"
+    }
+    
 
-assertObjectsEqual(actual1, expected1, 'detects that two objects are equal');
+    var actual = {
+        foo : "5",
+        bar : "6"
+    }
+    
 
-var expected2 = { foo: 6, bar: 5 };
-var actual2 = { foo: 5, bar: 6 };
-assertObjectsEqual(actual2, expected2, 'detects that two objects are equal');
+    assertObjectsEqual(actual, expected, 'detects that two objects are equal'); // Passed
+
+    var expected2 = {foo: 6,
+                        bar: 5};
+    assertObjectsEqual(actual, expected2, 'detects that two objects are equal'); // failed as expected is {"foo":6,"bar":5}, but got {"foo":5,"bar":6}
+
+}
+
+main()
